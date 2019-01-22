939dc77394e561a3a40d08837982efbaae402f51
Sergi Roca Tejada <anuvis.rt@gmail.com>
2019-01-22 23:54:45 +0100

    First commit

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public Rigidbody theRB;
+    public float jumpForce;
+
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y);
+        if(Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }
+    }
+}
