88f19da7d1de534d26ee50115231420e92aa62a5
Sergi Roca Tejada <anuvis.rt@gmail.com>
2019-01-23 01:37:12 +0100

    Caja arrastrar

---
+++ New Unity Project/Assets/Scripts/PlayerPush.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPush : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float distance = 1f;
+    public LayerMask boxMask;
+    GameObject box;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics2D.queriesStartInColliders = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+        if (hit.collider != null && Input.GetButtonDown("E"))
+        {
+            box = hit.collider.gameObject;
+        }
+    }
+
+}
